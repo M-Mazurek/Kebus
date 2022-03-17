bbf0fa596de336cebc259422413489802d4dff4e
Michał Mazurek <m.mazurekmail@gmail.com>
2022-03-17 23:12:41 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,11 @@
+﻿namespace Kebus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Добро пожаловать в кебус!");
+        }
+    }
+}
